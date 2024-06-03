faa0f813306625e90f3b3babfc9421d4e53ab99c
Russell Gordon <rgordon@lcs.on.ca>
2024-06-03 14:27:47 -0400

    Got a fixed or static version IntroView mostly done.

---
+++ MedsReminder/Views/IntroView.swift
@@ -0,0 +1,54 @@
+//
+//  IntroView.swift
+//  MedsReminder
+//
+//  Created by Russell Gordon on 2024-06-03.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        ZStack {
+            
+            Color.specialGreen
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Meds\nReminder")
+                    .foregroundStyle(Color.white)
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
+                
+                Image("IntroImage")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 50)
+                
+                Spacer()
+                
+                NavigationLink {
+                    Text("Another view")
+                } label: {
+                    Text("Get Started")
+                        .underline()
+                        .foregroundStyle(Color.white)
+                        .font(.largeTitle)
+                        .bold()
+
+                }
+
+
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    IntroView()
+}
