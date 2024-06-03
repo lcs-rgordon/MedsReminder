17d6494a6619c7c13165ebdc28dae531171e9438
Russell Gordon <rgordon@lcs.on.ca>
2024-06-03 14:09:30 -0400

    Initial Commit

---
+++ MedsReminder/MedsReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsReminderApp.swift
+//  MedsReminder
+//
+//  Created by Russell Gordon on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct MedsReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
